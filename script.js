2f76b82bef8453d630897d94e70c81a618209d54
cas1092 <castillo.goche.l@gmail.com>
2025-02-23 23:59:46 -0500

    final changes

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 const hamburgerMenu = document.querySelector('.hamburger-menu');
-const mainNav = document.querySelector('.navigation-bar');
+const navBarMobile = document.querySelector('.nav-bar-mobile');
+const closeButton = document.querySelector('.nav-bar-mobile .icon');
 
 hamburgerMenu.addEventListener('click', () => {
-    const isExpanded = mainNav.classList.contains('active');
-    mainNav.classList.toggle('active');
-    hamburgerMenu.setAttribute('aria-expanded', !isExpanded);
-    hamburgerMenu.setAttribute('aria-label', isExpanded ? 'Abrir menú de navegación' : 'Cerrar menú de navegación');
+    navBarMobile.classList.remove('hidden');
+    navBarMobile.classList.add('active'); // Agrega la clase 'active' para la transición
+    hamburgerMenu.setAttribute('aria-expanded', 'true');
+    hamburgerMenu.setAttribute('aria-label', 'Cerrar menú de navegación');
+});
+
+closeButton.addEventListener('click', () => {
+    navBarMobile.classList.remove('active'); // Quita la clase 'active' para la transición
+    setTimeout(() => {
+        navBarMobile.classList.add('hidden');
+    }, 300); // Espera a que termine la transición antes de ocultar
+    hamburgerMenu.setAttribute('aria-expanded', 'false');
+    hamburgerMenu.setAttribute('aria-label', 'Abrir menú de navegación');
 });
