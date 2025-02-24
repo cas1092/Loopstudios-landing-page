40232bcca0c71eb36876230da02e556d35564d72
cas1092 <castillo.goche.l@gmail.com>
2025-02-24 00:07:50 -0500

    some minor html changes

--- script.js
+++ script.js
@@ -6,14 +6,14 @@ hamburgerMenu.addEventListener('click', () => {
     navBarMobile.classList.remove('hidden');
     navBarMobile.classList.add('active'); // Agrega la clase 'active' para la transición
     hamburgerMenu.setAttribute('aria-expanded', 'true');
-    hamburgerMenu.setAttribute('aria-label', 'Cerrar menú de navegación');
+    
 });
 
 closeButton.addEventListener('click', () => {
     navBarMobile.classList.remove('active'); // Quita la clase 'active' para la transición
-    setTimeout(() => {
+    
         navBarMobile.classList.add('hidden');
-    }, 300); // Espera a que termine la transición antes de ocultar
+    
     hamburgerMenu.setAttribute('aria-expanded', 'false');
     hamburgerMenu.setAttribute('aria-label', 'Abrir menú de navegación');
 });
